c4328157a01ec79e59aaa1239f4c60bbd0ba8d39
BMateusSs <bmdss0810@gmail.com>
2026-08-29 19:18:54 -0300

    Model User

--- backend/app/models/categories.py
+++ backend/app/models/categories.py
@@ -2,12 +2,13 @@ from sqlalchemy import String, Text, text, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from uuid import UUID
 from sqlalchemy import UUID as SQLAlchemyUUID
-from fatetime import datetime
+from datetime import datetime
 
 from app.core.database import Base
 
 class Category(Base):
     __tablename__ = "categories"
+    
     id: Mapped[UUID] = mapped_column(SQLAlchemyUUID, 
                                      primary_key=True, 
                                      server_default=text("gen_random_uuid()"))
@@ -18,7 +19,6 @@ class Category(Base):
     
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
     
-    created_at: Mapped[datetime] = mapped_column(DateTime, 
-                                                 timezone=True, 
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), 
                                                  server_default=text("CURRENT_TIMESTAMP"))
     
---
+++ backend/app/models/users.py
@@ -0,0 +1,24 @@
+from sqlalchemy import String, Text, text, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from uuid import UUID
+from sqlalchemy import UUID as SQLAlchemyUUID
+from enum import Enum
+from sqlalchemy import Enum as SQLAlchemyEnum
+
+from app.core.database import Base
+
+class Role(str, Enum):
+    CUSTOMER = "CUSTOMER"
+    ADMIN = "ADMIN"
+
+class User(Base):
+    __tablename__  = "users"
+    
+    id: Mapped[UUID] = mapped_column(SQLAlchemyUUID, primary_key=True, server_default=text("gen_random_uuid()"))
+    username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[Role] = mapped_column(SQLAlchemyEnum(Role, name='role_type'), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+    
